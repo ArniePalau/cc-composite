82807e7dc697ab713c866148cfdae55a623114fe
Arnie Palau <52651583+ArniePalau@users.noreply.github.com>
2026-08-17 22:57:33 +0200

    style: add campaign art to report cards

--- tests/Package/PackageIntegrityTest.php
+++ tests/Package/PackageIntegrityTest.php
@@ -55,6 +55,7 @@ final class PackageIntegrityTest extends TestCase
         self::assertStringContainsString('Campanyes', file_get_contents($root . '/templates/frontend/field_report/index.html.twig'));
         self::assertStringContainsString('campaign.imagePath', file_get_contents($root . '/templates/frontend/field_report/index.html.twig'));
         self::assertStringContainsString('cc-report-card__campaign', file_get_contents($root . '/templates/frontend/field_report/index.html.twig'));
+        self::assertStringContainsString('cc-report-card--image', file_get_contents($root . '/templates/frontend/field_report/index.html.twig'));
     }
 
     public function testComposerUsesForumifyAvailableGdRenderer(): void
